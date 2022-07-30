f65b945a67fc26816c9ec2307dc60d23d209f07c
Manan Mistry <mananmistry07@gmail.com>
2022-07-31 01:35:10 +0530

    heroku test 2

--- src/main/java/com/example/quizapp/Config.java
+++ src/main/java/com/example/quizapp/Config.java
@@ -12,7 +12,7 @@ import java.net.URISyntaxException;
 public class Config {
         @Bean
         public DataSource getDataSource() throws URISyntaxException{
-            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+            URI dbUri = new URI("postgres://ozypvxwxfoqreo:4fe863838c01cfe701dd3d06a268897cb2abea0aa7f470545b26fadbdde7e09f@ec2-18-215-96-22.compute-1.amazonaws.com:5432/ddhmcc0r31sdfo");
             String username = dbUri.getUserInfo().split(":")[0];
             String password = dbUri.getUserInfo().split(":")[1];
             String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
